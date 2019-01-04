cddc3cecd18ddfb255a82e483fcb3928023e3cc7
Shashank Patibandla <shashank.patibandla@woodhouse.com>
2019-01-04 10:36:55 -0600

    SSL and Puma webserver

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-
+  post '/signup', to: 'users#create'
+  resources :users
 root 'static_pages#home'
 
 
