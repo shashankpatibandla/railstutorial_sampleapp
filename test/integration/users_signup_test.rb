4f3314c2a1b7c13fa0f1453692532874ea3a0232
Shashank Patibandla <shashank.patibandla@woodhouse.com>
2019-01-07 16:19:23 -0600

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,7 +12,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.nil?
-    #assert_template 'users/create'
+    assert is_logged_in?
   end
 end
