73bc51bc4c1b286cd49816910c8e7000bedffcde
Shashank Patibandla <shashank.patibandla@woodhouse.com>
2019-01-25 13:29:27 -0600

    trying it

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -20,7 +20,7 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
-    assert_match user.reset_token, mail.body.encoded
+    assert_match user.reset_token, mail.body.encoded #here I am
     assert_match CGI.escape(user.email), mail.body.encoded
   end
 end
