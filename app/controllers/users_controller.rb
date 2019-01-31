db8b0f41c9c3b19b647ed61d3fa01b826f22d516
Shashank Patibandla <shashank.patibandla@woodhouse.com>
2019-01-31 15:05:05 -0600

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless true
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -59,15 +59,6 @@ class UsersController < ApplicationController
 
   # Before filters
 
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
-
   # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
@@ -76,6 +67,6 @@ class UsersController < ApplicationController
 
   # Confirms an admin user.
   def admin_user
-  redirect_to(root_url) unless current_user.admin?
+    redirect_to(root_url) unless current_user.admin?
   end
 end
