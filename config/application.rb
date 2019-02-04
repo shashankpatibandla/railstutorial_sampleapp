1018cb199cf8c5655711eefe661a6ba03bb586ae
Shashank Patibandla <shashank.patibandla@woodhouse.com>
2019-02-04 13:00:37 -0600

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module SampleApp
     #config.time_zone = 'Central Time (US & Canada)'
     #config.active_record.default_timezone = :local
 
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,7 +48,7 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "emai addresses should be unique" do
+  test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
@@ -82,5 +82,34 @@ class UserTest < ActiveSupport::TestCase
     assert_difference 'Micropost.count', -1 do
       @user.destroy
      end
+  end
+
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+    # Posts from followed
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
   end
 end
